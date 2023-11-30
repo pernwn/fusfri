099b3319d2a852186eb7335f0651f5a172cb10c2
pernwn <pernillepjohansen@hotmail.com>
2023-11-30 14:52:15 +0100

    opsætning

---
+++ src/components/buttons.jsx
@@ -0,0 +1,7 @@
+export const Mainbtn = () => {
+    return(
+        //Knap component
+        <> 
+        </>
+    )
+}
---
+++ src/components/theme.jsx
@@ -0,0 +1,46 @@
+import { createTheme } from '@mui/material';
+
+
+export const fusfriTheme = (outerTheme) => 
+    createTheme({
+        palette: {
+            mode: 'light',
+            primary: {
+              main: '#6E8C2E',
+            },
+            secondary: {
+              main: '#3D7F8C',
+            },
+            background: {
+              default: '#D7E8ED',
+              paper: '#eef6f7',
+            },
+            text: {
+              primary: '#734434',
+            },
+          },
+          typography: {
+            fontFamily: 'Ubuntu',
+            h1: {
+              fontFamily: 'Varela Round',
+            },
+            h2: {
+              fontFamily: 'Varela Round',
+            },
+            h3: {
+              fontFamily: 'Varela Round',
+            },
+            h4: {
+              fontFamily: 'Varela Round',
+            },
+            h5: {
+              fontFamily: 'Varela Round',
+            },
+            h6: {
+              fontFamily: 'Varela Round',
+            },
+            button: {
+              fontFamily: 'Varela Round',
+            },
+          },
+});
