eee6da81c6735331c92c8bd4876aabadd541be8a
Rinamunk <eaarimun@students.eaaa.dk>
2023-12-01 13:28:52 +0100

    friskole indsat

---
+++ src/pages/friskole.jsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@mui/material";
+
+
+export default function friskole() {
+    return(
+        <ThemeProvider>
+
+
+
+            
+        </ThemeProvider>
+    )
+}
