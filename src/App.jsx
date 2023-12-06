b92cc81ed589e09984478e90ac8ff211b41ad1fa
pernwn <pernillepjohansen@hotmail.com>
2023-12-06 09:26:24 +0100

    fffffffa,d.f a., da,v d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider, useTheme } from "@emotion/react"
-import { Box, CssBaseline, Fab, Typography } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 import { myTheme } from "./components/theme";
 import { Navigate, Route, Routes } from "react-router-dom";
@@ -8,36 +8,13 @@ import HomePage from "./pages/homepage";
 import Nav from "./components/nav";
 import Friskole from "./pages/friskole";
 
-//IKONER FRA MUI
-import UpIcon from '@mui/icons-material/KeyboardArrowUp';
-import PhoneIcon from '@mui/icons-material/Phone';
-
-
 import './App.css'
 
-import { useEffect, useState } from "react";
 
 function App() {
   const outerTheme = useTheme();
-  
-  const [isScrolled, setScrolled] = useState(false);
-  
- const handleScroll = () => window.scrollY > 150 ? setScrolled(true) : setScrolled(false); 
- // handlescroll er også en faktor til noget infinite load eller langsom loading
-  
- const handleClick = () => {
- window.scrollTo(0,0);
- } 
- 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, []);
 
   return (
-
     <ThemeProvider theme={myTheme(outerTheme)}>
       <CssBaseline enableColorScheme />
 
@@ -50,24 +27,7 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} /* 404 */ />
       </Routes>
 
-       <Box sx={{ mr: 5 }} className={`action-btn-container ${isScrolled && 'action-btn-scrolled'}`}  > 
-        <Fab variant="extended" aria-label="contact">
-          <PhoneIcon sx={{ mr: 1 }} color="secondary" />
-          <Typography variant="button" sx={{ color: myTheme => myTheme.palette.secondary.main, fontWeight: "bold" }} >Kontakt</Typography>
-        </Fab>
-
-        <Fab size="small" color="secondary" aria-label="up" onClick={handleClick}>
-          <UpIcon />
-        </Fab>
-
-      </Box>
-
-
     </ThemeProvider>
-
-
-
-
   )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,11 +2,18 @@
 import { OutlinedBtn } from "./buttons";
 import { useEffect, useState } from "react";
 
+//IKONER FRA MUI
+import UpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PhoneIcon from '@mui/icons-material/Phone';
+
 //Individuel styling som udgangspunkt – sammensætter alt til sidst 
 import '../styles/vic.css'
 
 import logo from "../assets/logo/fusfri-logo.png"
-import { ButtonGroup } from "@mui/material";
+import { Box, ButtonGroup, Fab, Typography } from "@mui/material";
+
+
+
 
 export default function Nav() {
     /*This code defines a functional component Nav that displays a navigation bar for a website. 
@@ -28,20 +35,40 @@ export default function Nav() {
         }
     }, []);
 
+    const handleClick = () => {
+        window.scrollTo(0, 0);
+    }
+
     return (
-        <header className={`header-container ${isScrolled && 'header-scrolled'}`}>
-            <img src={logo} className={`logo ${isScrolled && 'logo-scrolled'}`} alt="Fussingø-Egnens Logo" />
+        <>
+            <header className={`header-container ${isScrolled && 'header-scrolled'}`}>
+                <img src={logo} className={`logo ${isScrolled && 'logo-scrolled'}`} alt="Fussingø-Egnens Logo" />
 
-            <ButtonGroup className={`nav-container ${isScrolled && 'nav-scrolled'}`}>
-                <OutlinedBtn name="Hjem" page="/" />
-                <OutlinedBtn name="Information" page="/information" />
-                <OutlinedBtn name="Om Fusfri" page="/omFus" />
-                <OutlinedBtn name="Kontakt" page="/kontakt" />
+                <ButtonGroup className={`nav-container ${isScrolled && 'nav-scrolled'}`}>
+                    <OutlinedBtn name="Hjem" page="/" />
+                    <OutlinedBtn name="Information" page="/information" />
+                    <OutlinedBtn name="Om Fusfri" page="/omFus" />
+                    <OutlinedBtn name="Kontakt" page="/kontakt" />
 
-                <OutlinedBtn name="Friskole" page="/friskole" />
-            </ButtonGroup>
+                    <OutlinedBtn name="Friskole" page="/friskole" />
+                </ButtonGroup>
 
-        </header>
+            </header>
+  
+                <Box sx={{ mr: 5 }} className={`action-btn-container ${isScrolled && 'action-btn-scrolled'}`}  >
+                    <Fab variant="extended" aria-label="contact">
+                        <PhoneIcon sx={{ mr: 1 }} color="secondary" />
+                        <Typography variant="button" sx={{ color: myTheme => myTheme.palette.secondary.main, fontWeight: "bold" }} >Kontakt</Typography>
+                    </Fab>
+
+                    <Fab size="small" color="secondary" aria-label="up" onClick={handleClick}>
+                        <UpIcon />
+                    </Fab>
+
+                </Box>
+
+
+        </>
     )
 
 }
