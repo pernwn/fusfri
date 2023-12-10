5f8c838a92e86245dd585d50de9813e13317a87a
pernwn <pernillepjohansen@hotmail.com>
2023-12-11 00:17:31 +0100

    Update friskole.jsx

--- src/pages/friskole.jsx
+++ src/pages/friskole.jsx
@@ -7,35 +7,35 @@ import gif from '../assets/gifgif.gif'
 export default function Friskole() {
     const postIDs = [28, 191, 149, 177, 179, 181, 183, 185, 187, 189];
 
-    return( 
+    return (
         <main>
             <Stack
-            sx={{
-                flexDirection: 'row',
-                padding: 10,
-                backgroundColor: '#ffff',
-                flexWrap: 'wrap',
+                sx={{
+                    flexDirection: 'row',
+                    padding: 10,
+                    backgroundColor: '#ffff',
+                    flexWrap: 'wrap',
 
-            }}
+                }}
             >
-            <img src={gif} className="gif-skole" ></img>
-            <Typography variant="h1" 
-              sx={{
-                textAlign:'center',
+                <img src={gif} className="gif-skole" ></img>
+                <Typography variant="h1"
+                    sx={{
+                        textAlign: 'center',
 
-              }}
-            > Friskolen</Typography>
-            <Stack
-             sx={{
-                textAlign:'center',
-              }}>
-            <Typography variant='h4'>Hos os foregår tingene anderledes end på en folkeskole, prøv at se en hverdag på vores lille skole!</Typography>
+                    }}
+                > Friskolen</Typography>
+                <Stack
+                    sx={{
+                        textAlign: 'center',
+                    }}>
+                    <Typography variant='h4'>Hos os foregår tingene anderledes end på en folkeskole, prøv at se en hverdag på vores lille skole!</Typography>
+                </Stack>
             </Stack>
-        </Stack>
             <Stack
                 sx={{
                     paddingTop: 5,
-                    paddingBottom:5,
+                    paddingBottom: 5,
                     overflowX: 'auto', // Enable horizontal scrolling
                     display: 'flex',
                     flexDirection: 'row', // Display items horizontally
@@ -48,17 +48,18 @@ export default function Friskole() {
                         sx={{
                             borderRadius: 8,
                             flex: '0 0 auto',
-                            maxWidth: 400,
                             p: 0.4,
+                        /*maxWidth: 400,*/
+
 
                         }}
                     >
-                        <WordPressFag postId={postID} />
-                    </Stack>
-                ))}
+                <WordPressFag postId={postID} />
             </Stack>
+                ))}
+        </Stack>
           
            
-        </main>
+        </main >
     );
 }
