44f335af311830a84e9995a2530415dd56542505
nra95 <noreskelund@gmail.com>
2023-12-15 07:58:02 +0100

    op

--- src/pages/calender.jsx
+++ src/pages/calender.jsx
@@ -11,26 +11,31 @@ export default function Calendar() {
   const [imageUrls, setImageUrls] = useState([]);
 
   useEffect(() => {
-    async function getData() {
-      try {
-        const response = await fetch("https://wordpress.vicw.dk/wp-json/wp/v2/posts/162");
-        const data = await response.json();
+    function getData() {
+      fetch("https://wordpress.vicw.dk/wp-json/wp/v2/posts/162")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Create a temporary DOM element to parse the HTML content
+          const tempElement = document.createElement("div");
+          tempElement.innerHTML = data.content.rendered;
 
-        // Create a temporary DOM element to parse the HTML content
-        const tempElement = document.createElement("div");
-        tempElement.innerHTML = data.content.rendered;
+          // Find all image elements within the HTML content
+          const imageElements = tempElement.querySelectorAll("img");
 
-        // Find all image elements within the HTML content
-        const imageElements = tempElement.querySelectorAll("img");
+          // Extract the image URLs
+          const urls = Array.from(imageElements).map((img) => img.src);
 
-        // Extract the image URLs
-        const urls = Array.from(imageElements).map((img) => img.src);
-
-        // Set the image URLs in the state
-        setImageUrls(urls);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+          // Set the image URLs in the state
+          setImageUrls(urls);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
     }
 
     getData();
