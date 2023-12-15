fe44efa7b044b9a9f60122e0ccf903dc032cae00
Rinamunk <eaarimun@students.eaaa.dk>
2023-12-15 23:58:06 +0100

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -9,6 +9,7 @@ export default function ErrorPage() {
         <h1>404 - Side ikke fundet</h1>
         <p>Beklager, den side du leder efter, kunne ikke findes.</p>
         <p>Gå tilbage til <Link to="/">hjemmesiden</Link>.</p>
+        <iframe src="https://lottie.host/embed/5d9f8adc-b487-4e51-947a-54c80f9a1bc0/ByKUzY0QSt.json"></iframe>
       </div>
     </div>
   );
